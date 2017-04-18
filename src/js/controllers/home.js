a7b865927d9e394d1cec62283766c4b5bc1f2c21
Joshua Bradford <jjyet@yahoo.com>
2017-04-18 12:18:02 -0400

    added proper pics and start, working on scroll anchors

--- src/js/controllers/home.js
+++ src/js/controllers/home.js
@@ -1,11 +1,20 @@
 
-function Home ($scope, $http, $state, $rootScope){
-  console.log('hello');
-  $scope.test = function(){
-    console.log('hellofromclick')
-  }
+function Home ($scope, $http, $state, $rootScope, $window, $location, $anchorScroll){
+  $scope.goToAbout = function(){
+    console.log("hey")
+    $location.hash('about');
+    $anchorScroll();
+  };
+  $scope.goToPortfolio = function(){
+    $location.hash('portfolio');
+    $anchorScroll();
+  };
+  $scope.goToContact = function(){
+    $location.hash('contact');
+    $anchorScroll();
+  };
 }
 
-Home.$inject = ['$scope', '$http', '$state', '$rootScope'];
+Home.$inject = ['$scope', '$http', '$state', '$rootScope', '$window', '$location', '$anchorScroll'];
 
 export default Home;
