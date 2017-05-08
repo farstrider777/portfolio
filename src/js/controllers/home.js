2a8ecdaa41fb1ff0fce87207136dca2435427a5e
Joshua Bradford <jjyet@yahoo.com>
2017-05-08 11:00:34 -0400

    added spacer for top, added more vertical space for title bars to account for small device stacking menu, added link to interviews in top menu bar

--- src/js/controllers/home.js
+++ src/js/controllers/home.js
@@ -1,19 +1,22 @@
 
 function Home ($scope, $http, $state, $rootScope, $window, $location, $anchorScroll){
   $scope.goToAbout = function(){
-    // $state.go('home')
     //$location.hash('about');
     $window.location.href = '#!/home#about';
     $anchorScroll();
   };
   $scope.goToPortfolio = function(){
-    // $state.go('home')
     // $location.hash('portfolio');
     $window.location.href = '#!/home#portfolio';
     $anchorScroll();
   };
+  $scope.goToInterviews = function(){
+    // $location.hash('interviews');
+    console.log("inside")
+    $window.location.href = '#!/home#interviews';
+    $anchorScroll();
+  };
   $scope.goToContact = function(){
-    // $state.go('home')
     // $location.hash('contact');
     $window.location.href = '#!/home#contact';
     $anchorScroll();
