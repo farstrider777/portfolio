0fe230e08822005968fa9c7a8e531009496ca012
Joshua Bradford <jjyet@yahoo.com>
2017-05-09 13:10:42 -0400

    set up first open and close ability, clean up other code, spaced the git hub links and arrow buttons with floats

--- src/js/controllers/home.js
+++ src/js/controllers/home.js
@@ -20,9 +20,48 @@ function Home ($scope, $http, $state, $rootScope, $window, $location, $anchorScr
     $window.location.href = '#!/home#contact';
     $anchorScroll();
   };
+  $scope.expand = 'closed';
+  $scope.arrowDirection = 'down';
   $scope.openShowRankerDes = function(){
-    console.log("test")
+    if ($scope.expand === 'closed')
+      $scope.expand = 'open';
+    else
+      $scope.expand = 'closed';
+
+    if ($scope.arrowDirection === 'down')
+      $scope.arrowDirection = 'up';
+    else
+      $scope.arrowDirection = 'down';
   };
+  $scope.expand2 = 'closed';
+  $scope.arrowDirection2 = 'down';
+  $scope.openWhackABugDes = function(){
+    console.log("test2")
+    if ($scope.expand2 === 'closed')
+      $scope.expand2 = 'open';
+    else
+      $scope.expand2 = 'closed';
+
+    if ($scope.arrowDirection2 === 'down')
+      $scope.arrowDirection2 = 'up';
+    else
+      $scope.arrowDirection2 = 'down';
+  };
+  $scope.expand3 = 'closed';
+  $scope.arrowDirection3 = 'down';
+  $scope.openEtsyDes = function(){
+    if ($scope.expand3 === 'closed')
+      $scope.expand3 = 'open';
+    else
+      $scope.expand3 = 'closed';
+
+    if ($scope.arrowDirection3 === 'down')
+      $scope.arrowDirection3 = 'up';
+    else
+      $scope.arrowDirection3 = 'down';
+  };
+
+
 }
 
 Home.$inject = ['$scope', '$http', '$state', '$rootScope', '$window', '$location', '$anchorScroll'];
